d38d55084f963ee9642c3c6d000ff1725dc6882d
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-01 15:37:57 +0200

    convert spherical to caterian

---
+++ hippospharm/utils.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+
+def transform_cartesian_to_spherical(x, y,z):
+    x = np.array(x)
+    y = np.array(y)
+    z = np.array(z)
+
+    r = np.sqrt(x ** 2 + y ** 2 + z ** 2)
+    r = np.where(r == 0, 0.0001, r)
+    theta = np.arccos(z * 1. / r)
+    phi = np.arctan2(y, x) + np.pi
+    return r, theta, phi

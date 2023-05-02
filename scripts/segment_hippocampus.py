30033503d13afe05baebc2df70f8b573b9f36282
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-02 22:15:10 +0200

    execute in subprocesses

--- scripts/segment_hippocampus.py
+++ scripts/segment_hippocampus.py
@@ -1,5 +1,6 @@
 import os
 
+import multiprocessing
 import tqdm
 from hippmapper.cli import main
 
@@ -23,7 +24,7 @@ files_hipp = [make_output(f_in, sub, 'hipp') for f_in, sub in zip(files_input, s
 # make a list of commands for bias correction
 commands = []
 for f_in, f_out in zip(files_input, files_corrected):
-    if not os.path.exists(f_out):hipp segmentation
+    if not os.path.exists(f_out):
         commands.append(['bias_corr', '-i', f_in, '-o', f_out])
 # run the commands
 for c in tqdm.tqdm(commands, desc='bias correction', total=len(commands)):
@@ -37,7 +38,29 @@ for f_in, f_out in zip(files_corrected, files_hipp):
 
 # run the commands
 for c in tqdm.tqdm(commands, desc='hipp segmentation', total=len(commands)):
-    main(c)
-# running the commnands apply each images?
-# args = ['--help']
-# main(args)
+    # time this loop
+    print('''\n
+        # ------------------------> Hippocampus segmentation <------------------------
+       ''')
+    import time
+    start_time = time.time()
+    # main(c)
+    # Create a multiprocessing Process object for my_function
+    def execute_command(*c):
+        print('command', c)
+        c = list(c)
+        main(c)
+    p = multiprocessing.Process(target=execute_command, args=(c))
+
+    # Start the process
+    p.start()
+
+    # Wait for the process to complete
+    p.join()
+
+    print('Process completed')
+    print("--- %s seconds ---" % (time.time() - start_time))
+    # nice footer with ascii art
+    print('''\n
+        # ------------------------------------------------------------------------------
+         ''')

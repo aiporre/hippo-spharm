2bb4ca586dca1f1fa719dea3f3c96332542be8d5
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-11 17:33:29 +0200

    analysis have arguments now :)

--- scripts/analysis.py
+++ scripts/analysis.py
@@ -1,9 +1,16 @@
 from hippospharm.clustering.learning import clustering
+import argparse
 
-
-def main():
-    clustering('data/features/features.csv', metadata_csv='data/metadata/participants.tsv', num_clusters=2,
-               labels='age', method='aecm-kmeans')
+def main(features, metadata, num_clusters, labels, method):
+    clustering(features, metadata_csv=metadata, num_clusters=num_clusters,labels=labels, method=method)
 
 if __name__ == '__main__':
-    main()
+    # make a arguments
+    parser = argparse.ArgumentParser(description='Clustering hippocampus surfaces')
+    parser.add_argument('--features', type=str, help='path to features csv file')
+    parser.add_argument('--metadata', type=str, help='path to metadata csv file')
+    parser.add_argument('--num_clusters', type=int, help='number of clusters', default=2)
+    parser.add_argument('--labels', type=str, help='column name in metadata to use as labels', default='age')
+    parser.add_argument('--method', type=str, help='clustering method', default='AECM-KMeans')
+    args = parser.parse_args()
+    main(args.features, args.metadata, args.num_clusters, args.labels, args.method)

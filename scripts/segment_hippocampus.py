2a0251e92ca9be20714a36c2aadff463ffbcd583
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-15 04:16:41 +0200

    segmentation uses the reoriented

--- scripts/segment_hippocampus.py
+++ scripts/segment_hippocampus.py
@@ -4,64 +4,34 @@ import multiprocessing
 import tqdm
 import time
 from hippmapper.cli import main
+import argparse
 
 
-def usage():
-    print("Usage python segmentation.py [options] <dataset with sub-dirs>")
-    print(" -h, --help display this message")
-    print(" -b, --brain apply only brain extraction i.e. brain.nii.gz, otherwise apply in corrected.nii.gz")
-    print(" -p, --processes <number of processes> number of processes to run in parallel, default is 1")
-
-if "-h" in sys.argv or "--help" in sys.argv:
-    usage()
-    sys.exit(0)
-
-if len(sys.argv) < 2:
-    print("Error: Directory is missing")
-    usage()
+# arg
+parser = argparse.ArgumentParser(description='hipp segmentation')
+parser.add_argument('dataset_path', type=str, help='dataset path')
+parser.add_argument('--processes', type=int, default=1, help='number of processes')
+parser.add_argument('-b', '--brain', action='store_true', help='use brain extraction, default is bias correction')
+parser.add_argument('-r', '--reoriented', action='store_true', help='use reoriented extraction, default is bias correction')
+args = parser.parse_args()
+dataset_path = args.dataset_path
+PROCESSES = max(1, multiprocessing.cpu_count()-1)
+processes = args.processes
+if processes == '-1':
+    processes = PROCESSES
+brain_extraction = args.brain
+reoriented = args.reoriented
+if brain and reoriented:
+    print('Error: brain and reoriented are mutually exclusive')
     sys.exit(0)
 
 # assert last argument is a directory
-dataset_path = sys.argv[-1]
 if not os.path.isdir(dataset_path):
     print("Error: Directory is missing", dataset_path, " is not a valid direcot")
-    usage()
-    sys.exit(0)
-
-brain_extraction = False
-processes = 1
-PROCESSES = max(1, multiprocessing.cpu_count()-1)
-if len(sys.argv) == 3:
-    if sys.argv[1] == '-b' or sys.argv[1] == '--brain':
-        dataset_path = sys.argv[-1]
-        print('brain extraction')
-        brain_extraction = True
-    elif sys.argv[1] == '-p' or sys.argv[1] == '--processes':
-        dataset_path = sys.argv[-1]
-        print('processes')
-        processes = PROCESSES
-    else:
-        print('Error: Unknown option')
-        usage()
-        sys.exit(0)
-elif len(sys.argv) == 4:
-    options = sys.argv[1:3]
-    assert '-b' in options or '--brain' in options, "Error: Unknown option"
-    assert '-p' in options or '--processes' in options, "Error: Unknown option"
-    dataset_path = sys.argv[-1]
-    brain_extraction = True
-    processes = PROCESSES
-elif len(sys.argv) == 2:
-    dataset_path = sys.argv[-1]
-    brain_extraction = False
-else:
-    print('Error: Unknown option')
-    usage()
     sys.exit(0)
 
 if not os.path.isdir(dataset_path):
     print("Error: Directory is missing", dataset_path, " is not a valid direcot")
-    usage()
     sys.exit(0)
 
 
@@ -77,10 +47,11 @@ def get_mri(sub):
     files = os.listdir(os.path.join(dataset_path,sub,'anat'))
     if brain_extraction:
         mri_file = [f for f in files if f.endswith('_brain.nii.gz')][0]
+    elif reoriented:
+        mri_file = [f for f in files if f.endswith('_reoriented.nii.gz')][0]
     else:
         mri_file = [f for f in files if f.endswith('_corrected.nii.gz')][0]
-    return os.path.join(dataset_path,sub, 'anat', mri_file)
-
+    return os.path.join(dataset_path, sub, 'anat', mri_file)
 print('one mri file' , get_mri(subs[0]))
 
 # make a list of inputs
@@ -93,20 +64,20 @@ def make_output(f_input, sub,  suffix):
     print('subs', sub)
     var_path = os.path.join(var_path,  sub + '_' + suffix + '.nii.gz')
     return var_path
-if brain_extraction:
-    files_corrected = [make_output(f_in, sub, 'brain') for f_in, sub in zip(files_input, subs)]
-else:
-    files_corrected = [make_output(f_in, sub, 'corrected') for f_in, sub in zip(files_input, subs)]
+# if brain_extraction:
+#     files_corrected = [make_output(f_in, sub, 'brain') for f_in, sub in zip(files_input, subs)]
+# else:
+#     files_corrected = [make_output(f_in, sub, 'corrected') for f_in, sub in zip(files_input, subs)]
 files_hipp = [make_output(f_in, sub, 'seg') for f_in, sub in zip(files_input, subs)]
 
 print('first file input,', files_input[0])
-print('first file corrected,', files_corrected[0])
+# print('first file corrected,', files_corrected[0])
 print('first file files_hipp,', files_hipp[0])
 
 
 # make a list of commands for hippocampus segmentation
 commands = []
-for f_in, f_out in zip(files_corrected, files_hipp):
+for f_in, f_out in zip(files_input, files_hipp):
     if not os.path.exists(f_out):
         commands.append(['seg_hipp', '-t1', f_in, '-o', f_out])
 

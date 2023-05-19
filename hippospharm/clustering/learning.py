abd6e714c4a4b4a1fa8976b5de518616d610b4fd
aiporre <ariel.iporre.rivas@gmail.com>
2023-05-19 19:55:32 +0200

    mod to make plots

--- hippospharm/clustering/learning.py
+++ hippospharm/clustering/learning.py
@@ -27,8 +27,8 @@ def clustering(feat_csv, method='PCA-Kmeans', num_clusters=2, labels=None, metad
     if metadata_csv is not None:
         feats = load_metadata(features=feats, metadata_csv=metadata_csv)
         # filter right or left
-        #side = 'left'
-        #feats = feats.loc[feats['side'] == side]
+        # side = 'left'
+        # feats = feats.loc[feats['side'] == side]
 
         # get labels
         if labels == 'age':
@@ -42,16 +42,34 @@ def clustering(feat_csv, method='PCA-Kmeans', num_clusters=2, labels=None, metad
             y = feats['side'].values
             # transform to 0 and 1 from L and R
             y = (y == 'left').astype('float32')
+        elif labels == 'all':
+            print('plotting all labels')
+            y = None
         else:
             y = feats['side'].values
     print(feats.head())
     # get features
     feat_cols = [c for c in feats.columns if 'feat' in c]
-    X = feats[feat_cols].values
+    X = feats[feat_cols].values[:, :500]
+    # y = feats['age'].values
+    # y = (y>30).astype('float32')
+    # mask_1 = y == 1
+    # mask_2 = y == 0
+    # center_1 = np.mean(X[mask_1], axis=0)
+    # center_2 = np.mean(X[mask_2], axis=0)
+    # # move random
+    # center_1 = center_1 + 10*np.ones(center_1.shape)
+    # center_2 = center_2 #+ np.random.normal(0, 0.1, center_2.shape)
+    # X1 = X[mask_1] - center_1
+    # X2 = X[mask_2] - center_2
+    # X = np.concatenate([X1, X2], axis=0)
+    # y = np.concatenate([y[mask_1], y[mask_2]], axis=0)
+    # feats['age'] = y*50 + 25
+    # y=None
 
     # create embedding
     emb = Embedding(method=method, dims=dims, num_clusters=
-            num_clusters)
+            num_clusters, epochs=1000)
     z, clusters = emb.fit(X, y=y)
     # plot
     if labels is None:
@@ -61,6 +79,39 @@ def clustering(feat_csv, method='PCA-Kmeans', num_clusters=2, labels=None, metad
             plt.scatter(z[clusters ==i,0], z[clusters ==i,1], label='cluster {}'.format(i))
         plt.legend()
         plt.show()
+    elif labels == 'all':
+        fig, ax = plt.subplots(1,3, figsize=(15,5), layout='constrained')
+        cmap = plt.get_cmap('jet')
+        norm = plt.Normalize(clusters.min(), clusters.max())
+        for c in np.unique(clusters):
+            mask = clusters == c
+            # ax[0].scatter(z[mask,0], z[mask,1], c=cmap(norm(c)), label='cluster {}'.format(c))
+            ax[0].scatter(z[mask, 0], z[mask, 1], label='{}'.format(c))
+        ax[0].legend(loc='upper right',bbox_to_anchor=(-0.15, 1.05),
+                     fancybox=True, shadow=True, ncol=1)
+
+        # ax[0].scatter(z[:,0], z[:,1], c=clusters)
+        y = feats['age'].values
+        s = ax[1].scatter(z[:,0], z[:,1], c=y)
+        plt.colorbar(s, ax=ax[1], location='bottom')
+
+
+        # for age in np.unique(y):
+        #     mask = y == age
+        #     ax[1].scatter(z[mask,0], z[mask,1], c=cmap(norm(age)), label='age {}'.format(age))
+        # ax[1].legend()
+        # ax[1].set_title('age')
+        # plot sex
+        y = feats['sex']
+        y = (y == 'M').astype('float32')
+        mask_male = y == 1
+        ax[2].scatter(z[mask_male,0], z[mask_male,1], c='b', label='M')
+        mask_female = y == 0
+        ax[2].scatter(z[mask_female,0], z[mask_female,1], c='r', label='F')
+        ax[2].legend()
+        # plt.colorbar(ax=ax)
+        plt.show()
+
     else:
         fig, ax = plt.subplots(1,2, figsize=(10,5))
         for i in range(num_clusters):
